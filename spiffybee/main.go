ba41265a5bf9f8ddeb2055a150bf1bb45ba7dca8
hieutrpham <hieutrungpham91@gmail.com>
2025-11-24 16:39:18 +0200

    add helper functions and just exploring the code base

--- spiffybee/main.go
+++ spiffybee/main.go
@@ -8,23 +8,86 @@ import (
 
 import . "hive-arena/common"
 
+var hive_coord = Coords{Row: 0, Col: 0}
 var dirs = []Direction{E, SE, SW, W, NW, NE}
 
-func think(state *GameState, player int) []Order {
+func is_entity_type(hex Hex, unit_type EntityType, player int) bool {
+	unit := hex.Entity
+	return unit != nil && unit.Type == unit_type && unit.Player == player
+}
 
+func is_terrain_type(hex Hex, terrain_type Terrain) bool {
+	terrain := hex.Terrain
+	return terrain == terrain_type
+}
+
+func can_forage(hex Hex) bool {
+	return is_terrain_type(hex, FIELD) && !hex.Entity.HasFlower
+}
+
+func get_hive(state *GameState, player int) {
+	for coords, hex := range state.Hexes {
+		if is_entity_type(*hex, HIVE, player) {
+			hive_coord.Row = coords.Row
+			hive_coord.Col = coords.Col
+		}
+	}
+}
+
+func get_hex_type(state *GameState, coord Coords, player int) []string {
+	hex := state.Hexes[coord]
+	var arr []string
+	if hex != nil {
+		entity := hex.Entity
+		if entity != nil {
+			arr = []string{string(hex.Terrain), string(entity.Type)}
+		} else {
+			arr = []string{string(hex.Terrain), "nil"}
+		}
+		return arr
+	}
+	return nil
+}
+
+func think(state *GameState, player int) []Order {
 	var orders []Order
 
-	for coords, hex := range state.Hexes {
-		unit := hex.Entity
+	// for coords, hex := range state.Hexes {
+	// 	unit := hex.Entity
+	// 	terrain := hex.Terrain
+	// 	fmt.Print("coords: ", coords, " terrain type: ", terrain)
+	// 	if unit != nil && unit.Player == player {
+	// 		fmt.Println(" entity type: ", unit.Type)
+	// 	} else {
+	// 		fmt.Println()
+	// 	}
+	// }
 
-		if unit != nil && unit.Type == BEE && unit.Player == player {
-			// fmt.Println(coords, unit)
-			orders = append(orders, Order{
-				Type:      MOVE,
-				Coords:    coords,
-				Direction: dirs[rand.Intn(len(dirs))],
-				// Direction: dirs[0],
-			})
+	if hive_coord.Row == 0 && hive_coord.Col == 0 {
+		get_hive(state, player)
+	}
+
+	for coords, hex := range state.Hexes {
+		for _, v := range coords.Neighbours() {
+			arr := get_hex_type(state, v, player)
+			if arr != nil {
+				fmt.Println(arr)
+			}
+		}
+
+		if is_entity_type(*hex, BEE, player) {
+			if can_forage(*hex) {
+				orders = append(orders, Order{
+					Type:   FORAGE,
+					Coords: coords,
+				})
+			} else {
+				orders = append(orders, Order{
+					Type:      MOVE,
+					Coords:    coords,
+					Direction: dirs[rand.Intn(len(dirs))],
+				})
+			}
 		}
 	}
 
